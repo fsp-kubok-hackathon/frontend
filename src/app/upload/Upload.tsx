e941524bb7d7f67a3f62b45662610c1ea21d9433
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-04-26 17:21:00 +0300

    wip

--- src/app/upload/Upload.tsx
+++ src/app/upload/Upload.tsx
@@ -2,16 +2,46 @@
 import React from 'react';
 import ReceiptUpload from '@/components/receipt-upload';
 import { Button } from '@/components/ui/button';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ReceiptUploadFormSchema } from '@/lib/forms/upload.form';
+import { FormField } from '@/components/ui/form';
 
 export default function Upload() {
+  const form = useForm({
+    resolver: zodResolver(ReceiptUploadFormSchema),
+  });
+
+  // Отслеживаем содержимое receipts
+  const receipts = form.watch('receipts');
+
+  const onSubmit: SubmitHandler<any> = (data) => {
+    console.log(data);
+    // TODO:
+  };
+
   return (
     <>
       <div className="flex min-h-screen items-center justify-center flex-col p-20">
-        <div className="flex flex-col space-y-4">
-          <h1 className="text-4xl font-bold">Загрузка отчетности</h1>
-          <ReceiptUpload />
-          <Button>Отправить</Button>
-        </div>
+        <FormProvider {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col space-y-4"
+          >
+            <h1 className="text-4xl font-bold">Загрузка отчетности</h1>
+            <FormField
+              control={form.control}
+              name="receipts"
+              render={({ field }) => {
+                console.log(field);
+                return <ReceiptUpload {...field} />;
+              }}
+            />
+            <Button type="submit" disabled={!receipts || receipts.length === 0}>
+              Отправить
+            </Button>
+          </form>
+        </FormProvider>
       </div>
     </>
   );
--- src/components/receipt-upload/index.tsx
+++ src/components/receipt-upload/index.tsx
@@ -1,4 +1,4 @@
-import { useId, useState } from 'react';
+import { forwardRef, useEffect, useId, useState } from 'react';
 import { PlusCircle, TrashIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
@@ -20,71 +20,85 @@ import {
   TableRow,
 } from '@/components/ui/table';
 
+interface ReceiptUploadProps {
+  value: FileWithPreview[];
+  onChange: (files: FileWithPreview[]) => void;
+}
+
 // Тип для объекта файла
-interface FileWithPreview extends File {
-}
+interface FileWithPreview extends File {}
 
-export default function ReceiptUpload() {
-  const [files, setFiles] = useState<FileWithPreview[]>([]);
+const ReceiptUpload = forwardRef(
+  ({ value, onChange }: ReceiptUploadProps, ref) => {
+    const [files, setFiles] = useState<FileWithPreview[]>(value || []);
 
-  const handleAddFiles = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      const fileList = Array.from(event.target.files).map((file) => file);
-      console.log({ ...event.target.files[0] });
-      setFiles([...files, ...fileList]);
-    }
-  };
+    // Обновляем форму при изменении файлов
+    useEffect(() => {
+      onChange(files);
+    }, [files, onChange]);
 
-  const handleDeleteFile = (index: number) => {
-    setFiles(files.filter((_, i) => i !== index));
-  };
+    const handleAddFiles = (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (event.target.files) {
+        const fileList = Array.from(event.target.files).map((file) => file);
+        setFiles([...files, ...fileList]);
+      }
+    };
 
-  const fileUploadId = useId();
+    const handleDeleteFile = (index: number) => {
+      setFiles(files.filter((_, i) => i !== index));
+    };
 
-  return (
-    <Card className="min-w-[400px]">
-      <CardHeader>
-        <CardTitle>Фотографии</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Фото</TableHead>
-              <TableHead>Действия</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {files.map((file, index) => (
-              <TableRow key={index}>
-                <TableCell>{file.name}</TableCell>
-                <TableCell>
-                  <Button onClick={() => handleDeleteFile(index)}>
-                    <TrashIcon className="h-3.5 w-3.5" />
-                  </Button>
-                </TableCell>
+    const fileUploadId = useId();
+
+    return (
+      <Card className="min-w-[400px]">
+        <CardHeader>
+          <CardTitle>Фотографии чеков</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Фото</TableHead>
+                <TableHead>Действия</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </CardContent>
-      <CardFooter className="justify-center border-t p-4">
-        <label htmlFor={fileUploadId}>
-          <Button asChild size="sm" variant="ghost" className="gap-1">
-            <span>
-              <PlusCircle className="h-3.5 w-3.5" />
-              Добавить фото
-            </span>
-          </Button>
-        </label>
-        <Input
-          id={fileUploadId}
-          type="file"
-          multiple
-          onChange={handleAddFiles}
-          style={{ display: 'none' }}
-        />
-      </CardFooter>
-    </Card>
-  );
-}
+            </TableHeader>
+            <TableBody>
+              {files.map((file, index) => (
+                <TableRow key={index}>
+                  <TableCell>{file.name}</TableCell>
+                  <TableCell>
+                    <Button onClick={() => handleDeleteFile(index)}>
+                      <TrashIcon className="h-3.5 w-3.5" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+        <CardFooter className="justify-center border-t p-4">
+          <label htmlFor={fileUploadId}>
+            <Button asChild size="sm" variant="ghost" className="gap-1">
+              <span>
+                <PlusCircle className="h-3.5 w-3.5" />
+                Добавить фото
+              </span>
+            </Button>
+          </label>
+          <Input
+            id={fileUploadId}
+            type="file"
+            multiple
+            onChange={handleAddFiles}
+            style={{ display: 'none' }}
+          />
+        </CardFooter>
+      </Card>
+    );
+  },
+);
+
+ReceiptUpload.displayName = 'ReceiptUpload';
+
+export default ReceiptUpload;
