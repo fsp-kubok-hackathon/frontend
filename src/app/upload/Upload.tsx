6330e9686a44c47fd8b6f44e199e616d7f28081e
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-04-26 16:54:50 +0300

    add layout for upload page

--- src/app/upload/Upload.tsx
+++ src/app/upload/Upload.tsx
@@ -1,12 +1,17 @@
 'use client';
 import React from 'react';
 import ReceiptUpload from '@/components/receipt-upload';
+import { Button } from '@/components/ui/button';
 
 export default function Upload() {
   return (
     <>
-      <div className="flex min-h-screen items-center justify-center p-20">
-        <ReceiptUpload />
+      <div className="flex min-h-screen items-center justify-center flex-col p-20">
+        <div className="flex flex-col space-y-4">
+          <h1 className="text-4xl font-bold">Загрузка отчетности</h1>
+          <ReceiptUpload />
+          <Button>Отправить</Button>
+        </div>
       </div>
     </>
   );
